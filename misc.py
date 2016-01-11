234290ee93a75e7bf17ea990b81264fcb35d8042
pavdmyt <mail@pavdmyt.com>
2016-01-11 16:57:15 +0200

    `is_prime` 6k+-1 optimization implemented. Tests updated.

--- misc.py
+++ misc.py
@@ -8,16 +8,16 @@ def is_prime(num):
     """
     Checks that given number is a prime number.
     """
-    if num > 1:
-        if num == 2:
-            return True
-        if num % 2 == 0:
-            return False
-        for i in range(3, int(math.sqrt(num)) + 1, 2):
-            if num % i == 0:
-                return False
+    if num <= 1:
+        return False
+    elif num <= 3:
         return True
-    return False
+    elif num % 2 == 0 or num % 3 == 0:
+        return False
+    for i in range(5, int(math.sqrt(num)) + 1, 6):
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+    return True
 
 
 # py.test
@@ -26,8 +26,12 @@ def test_primes():
     assert not is_prime(-5)
     assert not is_prime(0)
     assert not is_prime(1)
+    assert not is_prime(125)
 
     # Primes.
     assert is_prime(2)
+    assert is_prime(3)
     assert is_prime(31)
     assert is_prime(56509)
+    assert is_prime(179425879)
+    assert is_prime(32416188583)
