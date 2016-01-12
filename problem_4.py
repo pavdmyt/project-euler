8faee1239868ab61884a83a5990e255ba1df534c
pavdmyt <mail@pavdmyt.com>
2016-01-12 17:58:45 +0200

    Problem 4. Simplified.

--- problem_4.py
+++ problem_4.py
@@ -4,25 +4,10 @@ http://static.projecteuler.net/problem=4
 """
 
 
-def reverse_num(num):
-    """
-    Reverse given number.
-    123 -> 321
-    """
-    lst = [digit for digit in str(num)]
-    return int(''.join(lst[::-1]))
-
-
-def is_palindrome(num):
-    """
-    Checks if number is a palindrome.
-    """
-    return num == reverse_num(num)
-
-
 def main():
     rng = range(100, 1000)
     products_set = {i*j for i in rng for j in rng}
+    is_palindrome = lambda num: str(num) == str(num)[::-1]
     palindromes = filter(is_palindrome, products_set)
     print(max(palindromes))
 
