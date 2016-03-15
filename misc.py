d7d75332af33b9fc5d26637ba7321da243722e17
pavdmyt <mail@pavdmyt.com>
2016-03-15 17:37:24 +0200

    `is_prime` refactored.

--- misc.py
+++ misc.py
@@ -8,12 +8,9 @@ def is_prime(num):
     """
     Checks that given number is a prime number.
     """
-    if num <= 1:
-        return False
-    elif num <= 3:
-        return True
-    elif num % 2 == 0 or num % 3 == 0:
-        return False
+    if num <= 3:
+        return num == 2 or num == 3
+
     for i in range(5, int(math.sqrt(num)) + 1, 6):
         if num % i == 0 or num % (i + 2) == 0:
             return False
